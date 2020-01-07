61396253408e7fcbdd40855f4b54d6768a200c30
tramvay5401 <tramvay5401@gmail.com>
2020-01-07 23:15:12 +0200

    end project

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,39 @@
 $(function() {
- 
+  $(".header__burger").on("click", function(event) {
+    $(".header__burger,.header__menu").toggleClass("active");
+  });
+  
+  
+  $(".product__content-tabs-wrapper .tab ").on("click", function(event) {
+    var id = $(this).attr("data-id");
+    $(".product__content-tabs-wrapper, .settings__tabs")
+      .find(".tab-item")
+      .removeClass("active-tab")
+      .hide();
+    $(".product__content-tabs-wrapper .tabs, .settings__tabs .tabs")
+      .find(".tab")
+      .removeClass("active");
+    $(this).addClass("active");
+    $("#" + id)
+      .addClass("active-tab")
+      .fadeIn();
+    return false;
+  });
+
+  $('.preloader').fadeOut('slow');
+    $('.chart').each(function(){
+    $(this).easyPieChart({
+            size:140,
+            animate: 2000,
+            lineCap:'butt',
+            scaleColor: false,
+            barColor: '#fff',
+            trackColor: 'transparent',
+            lineWidth: 10
+        });
+    });
+
+  
+   
 });
+
